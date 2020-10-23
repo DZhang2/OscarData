20269c6759a9bbc7e5fb71cbea39b008db554865
DZhang2 <davidzhang312@gmail.com>
2020-10-22 21:12:34 -0400

    update

--- course_info.py
+++ course_info.py
@@ -1,37 +1,79 @@
 import requests
 import re
 import json
+import csv
 from bs4 import BeautifulSoup
 
-base_url = "http://catalog.gatech.edu"
 url = "http://catalog.gatech.edu/coursesaz/"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-courses = soup.find('div', id="atozindex")
-links = courses.find_all('a', href=True)
-departments = [link.text for link in links]
-hrefs = [link['href'] for link in links]
-hrefs = [base_url + href for href in hrefs]
+base_url = "http://catalog.gatech.edu"
 
-# departments = full department name + (abbreviated) in list format
-# href = links to department page with course info
-
-courses_info = {}
-for department in hrefs:
-    page = requests.get(department)
+def get_hrefs():
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    courses = soup.find_all(class_='courseblock')
-    dept_info = []
-    for course in courses:
-        course_title = course.find(class_='courseblocktitle').text
-        course_desc = course.find(class_='courseblockdesc').text  
-        identifier = course_title.split('.')[0].strip()
-        identifier = identifier.replace('\xa0', ' ')
-        credits = course_title.split('.')[2].strip().split()[0] if len(course_title.split('.')[2].strip()) > 0 else course_title.split('.')[3].strip()[0]
-        description = re.sub(r'[^a-zA-Z0-9 \.\\/\$\+\*\(\)\?\{\}-]', "", course_desc).strip()
-        course_info = {"course": identifier, "name": course_title.split('.')[1].strip(), "credits": credits, "description": description}
-        dept_info.append(course_info)
-    courses_info[department[36:-1].upper()] = dept_info
+    courses = soup.find('div', id="atozindex")
+    links = courses.find_all('a', href=True)
+    hrefs = [link['href'] for link in links]
+    hrefs = [base_url + href for href in hrefs]
+    return hrefs
 
-json.dump(courses_info, open('course_data.json', 'w'))
+def generate_course_data():
+    hrefs = get_hrefs()
+    # generate_json(hrefs)
+    generate_csv(hrefs)
+
+def generate_json(hrefs):
+    courses_info = {}
+    for department in hrefs:
+        page = requests.get(department)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        courses = soup.find_all(class_='courseblock')
+        dept_info = []
+        for course in courses:
+            course_title = course.find(class_='courseblocktitle').text.strip()
+            course_desc = course.find(class_='courseblockdesc').text 
+            identifier = course_title.split('.')[0].strip()
+            identifier = identifier.replace('\xa0', ' ')
+            name = course_title.split('.')[1:-2]
+            course_name = ""
+            for n in name:
+                course_name += n
+            credits = course_title.split('.')[-2].split()[0]
+            description = re.sub(r'[^a-zA-Z0-9\s\.\\/\$\+\*\(\)\?\{\}-]', "", course_desc).strip()
+            description = description.replace("\'", r"\'")
+            course_info = {"course": identifier, "name": course_name, "credits": credits, "description": description}
+            dept_info.append(course_info)
+        courses_info[department[36:-1].upper()] = dept_info
+    json.dump(courses_info, open('course_data.json', 'w'))
+
+def generate_csv(hrefs):
+    course_info = []
+    for department in hrefs:
+        page = requests.get(department)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        courses = soup.find_all(class_='courseblock')
+        for course in courses:
+            course_title = course.find(class_='courseblocktitle').text.strip()
+            course_desc = course.find(class_='courseblockdesc').text 
+            identifier = course_title.split('.')[0].strip()
+            identifier = identifier.replace('\xa0', ' ')
+            name = course_title.split('.')[1:-2]
+            course_name = ""
+            for n in name:
+                course_name += n
+            course_name = course_name.replace("\'", r"\'").strip()  # needed for sql
+            course_name = course_name.replace(",", " ")    #remove , for sql bulk insert purposes
+            credits = course_title.split('.')[-2].split()[0]
+            credits = credits.replace(",", "-")    
+            if not credits[0].isnumeric():
+                print(f"We have something weird here course title is: {course_title}")
+            description = re.sub(r'[^a-zA-Z0-9\s\.\\/\$\+\*\(\)\?\{\}-]', "", course_desc).strip()
+            description = description.replace("\'", r"\'")
+            description = description.replace(",", "")
+            course_info.append([identifier, course_name, credits, description])
+    with open('course_data.csv', 'w') as fout:
+        writer = csv.writer(fout)
+        writer.writerows(course_info)
+
+
+generate_course_data()
     
